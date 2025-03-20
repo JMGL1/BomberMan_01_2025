97556a1edec729b3cb92f79dc2c995aa534929e8
Carlos Walter Pacheco Lora <cwpl@hotmail.com>
2025-03-20 13:00:03 -0400

    implementacion TArray 20%

---
+++ Source/BomberMan_012025/BoqueMadera.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bloque.h"
+#include "BoqueMadera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBERMAN_012025_API ABoqueMadera : public ABloque
+{
+	GENERATED_BODY()
+	
+};
